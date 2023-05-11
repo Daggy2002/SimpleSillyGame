369c4bcdd46fd256bd3a58b33a7de909a0ec0254
Muhammed <momosuli8@gmail.com>
2023-05-11 04:50:42 +0200

    Lots of changes

--- Game.py
+++ Game.py
@@ -1,10 +1,12 @@
 import pygame
 import random
 import time
+import math
 
 screen = pygame.display.set_mode((1080, 720))
 
 pygame.font.init()
+pygame.mixer.init()
 # set up the font
 font = pygame.font.SysFont(None, 64)
 
@@ -27,7 +29,7 @@ player1_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 player2_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
 # Random x position and y position for the rectangle
-xRand = random.randint(1, screen.get_width())
+xRand = random.randint(0, screen.get_width())
 yRand = random.randint(1, screen.get_height())
 
 #Keep track of when the game started, so wwe can see how many seconds elapsed
@@ -42,6 +44,24 @@ speed = 2
 level = 0
 randXArray = []
 firstRUN = True
+OrbTime = 600
+OrbCord = (random.randint(0-60, screen.get_width()+60),random.randint(1, screen.get_height()))
+powerTime = 600
+displayOrb = False
+gotPower = False
+count1 =  0
+count2 =  0
+
+
+
+# Load the background music
+pygame.mixer.music.load("backgroundTrack.mp3")
+
+# Start playing the background music
+pygame.mixer.music.play(-1)  # -1 means loop indefinitely
+
+
+pygame.mixer.music.set_volume(1.5)
 
 while running:
     # poll for events
@@ -58,14 +78,15 @@ while running:
     while firstRUN == True:
         firstRUN = False
         for i in range(6):
-            x = random.randint(1, screen.get_width())
+            x = random.randint(0-60, screen.get_width()-60)
             randXArray.append(x)
-            print(randXArray)
+            #print(randXArray)
 
     
     #This is your player
     player1 = pygame.draw.circle(screen, "green", player1_pos, 20)
-    #player2 = pygame.draw.circle(screen, "blue",player2_pos, 20)
+    #time_control_orb = 0
+    
 
     #Timing
     current = time.perf_counter()
@@ -84,11 +105,28 @@ while running:
 
     #If he has, then speed them up
     if time_elapsed >= timeLevel1 and Level2 ==False:
+        
         level = 1
         for i in range(level+3):
             rectangle_obs = pygame.draw.rect(screen,"red", (randXArray[i], yRand, 60, 120))
             rectArray.append(rectangle_obs)
         speed = 4
+        
+        #generate random number to see if orb is visible
+        prob = random.randint(1,60)
+        print(prob)
+        if (prob == 10 and count1 <= 1):
+            count1+=1
+            print(True)
+            displayOrb = True
+            OrbTime = 600
+
+        if(OrbTime > 0 and displayOrb == True):
+            global time_control_orb
+            time_control_orb = pygame.draw.circle(screen, "gold",OrbCord, 20)  
+            OrbTime-=1
+            if(OrbTime == 1):
+                displayOrb = False
         #first time around, prompt user
         if Level1 == False:
             screen.blit(font.render("Level 1", True, "white"), text_rect)
@@ -97,7 +135,7 @@ while running:
             Level1 = True
 
         #time remaining thing
-        time_remaining = str(round(30 - time_elapsed+60,0))
+        time_remaining = str(round(30 - time_elapsed+30,0))
         screen.blit(font.render(time_remaining, True, (255, 255, 255)),(10, 10))
         yRand += speed
 
@@ -108,6 +146,19 @@ while running:
             rectangle_obs = pygame.draw.rect(screen,"red", (randXArray[i], yRand, 60, 120))
             rectArray.append(rectangle_obs)
         speed = 6
+
+        #generate random number to see if orb is visible
+        if ((random.randint(10,10) == 10) and count2 <=2):
+            count2+=1
+            OrbTime = 600
+            displayOrb = True
+
+        if(OrbTime > 0 and displayOrb == True):
+            time_control_orb = pygame.draw.circle(screen, "gold",OrbCord, 20)  
+            OrbTime-=1
+            if(OrbTime == 1):
+                displayOrb = False
+
         if Level2 == False:
             screen.blit(font.render("Level 2", True, "white"), text_rect)
             pygame.display.update()
@@ -125,49 +176,78 @@ while running:
         for i in range(level + 1):
             x = random.randint(1, screen.get_width())
             randXArray[i] = x
-            print(randXArray)
+            #print(randXArray)
         yRand = 0
+
+    if (displayOrb == True):
+        # Calculate the distance between the circles
+        distance = math.sqrt((player1.x-time_control_orb.x)**2 + (player1.y-time_control_orb.y)**2)
+
+   
+        # Check collision
+        if distance <= 20*2:
+            sound = pygame.mixer.Sound("power2.mp3")
+            sound.play()
+            gotPower = True
+            #print("Collision detected!")
+            displayOrb = False
+
 
     # determine if the space bar is held down
     slow_motion = False
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_SPACE]:
-        slow_motion = True
+    if (gotPower == True):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE]:
+            sound = pygame.mixer.Sound("power2.mp3")
+            sound.play()
+            slow_motion = True
 
-    # sets delta and the yRand to slow down time
-    if slow_motion:
-        dt = 0.0045
-        yRand-= speed*0.5
-    else:
-        dt = 0.009
+        # sets delta and the yRand to slow down time
+        if slow_motion:
+            dt = 0.0045
+            yRand-= speed*0.5
+            powerTime -=1
+            if(powerTime == 0):
+                gotPower = False
+        else:
+            dt = 0.009
+        
 
+    
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
-        if player1_pos.y > 0:
+        if player1_pos.y - 20 > 0:
             player1_pos.y -= 500 * dt
     if keys[pygame.K_s]:
-        if player1_pos.y < screen.get_height():
+        if player1_pos.y + 20 < screen.get_height():
             player1_pos.y += 500 * dt
     if keys[pygame.K_a]:
-        if player1_pos.x > 0:
+        if player1_pos.x - 20 > 0:
             player1_pos.x -= 500 * dt
     if keys[pygame.K_d]:
-        if player1_pos.x < screen.get_width():
+        if player1_pos.x + 20 < screen.get_width():
             player1_pos.x += 500 * dt
 
     
     for i in rectArray:
-        print("rectangle:" + str(i))
+        #print("rectangle:" + str(i))
         #Checks for a collions between player and enemy, user dies
         if (player1.colliderect(i)):
+            sound = pygame.mixer.Sound("death.mp3")
+            sound.play()
+            #pygame.time.wait(int(sound.get_length() * 1000))
             screen.blit(text_surface, text_rect)
             pygame.display.update()
         
             #Reset the start time, so he has to last the full amount to get to the level
             start = time.perf_counter()
 
-            xRand = random.randint(1, screen.get_width())
+            randXArray = []
+            for i in range(6):
+                x = random.randint(0-60, screen.get_width()-60)
+                randXArray.append(x)
 
+            yRand = 0
             pygame.time.wait(500)
 
             #reset their coordinates
